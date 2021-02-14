e830f07bc71dea0d10ac5ddd440fe7206ab9b75d
CHIRAG-CELL274 <78537868+CHIRAG-CELL274@users.noreply.github.com>
2021-02-14 16:35:27 +0530

    Add files via upload

---
+++ WEEK 2 QUESTION 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n,r,s;
+    printf("enter a no:");
+
+    scanf("%d",&n);
+
+    while(n!=0)
+    {
+        r=n%10;
+        s=s+r;
+        n=n/10;
+
+
+        
+
+
+
+    }
+
+    printf("%d",s);
+
+}
